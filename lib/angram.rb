6f2d2099eb88c8ba7d2ae611e3d3a0198d48af8e
Riki Montgomery <mostriki820@gmail.com>
2017-09-08 11:27:46 -0700

    test some methods on anagrams

--- lib/angram.rb
+++ lib/angram.rb
@@ -1,7 +1,25 @@
 #!/usr/bin/env ruby
 
 class Anagrams
-  def angram
+  def angram (input, comparison)
+    input = input.downcase.gsub(/[^a-z0-9]/i, '').split(//)
+    comparison = comparison.downcase.gsub(/[^a-z0-9]/i, '').split(//)
 
+    inputArr = input.sort
+    comparisonArr = comparison.sort
+
+    if inputArr == comparisonArr
+      return true
+    else
+      return false
   end
 end
+
+
+input = "I'm a dot in place.".downcase.gsub(/[^a-z0-9]/i, '').split(//)
+inputArr = input.sort
+
+comparison = "Bury".downcase.split(//)
+comparisonArr = comparison.sort
+
+"I'm a dot in place.".tr(/[^a-z0-9]/, '').downcase.split(//)
