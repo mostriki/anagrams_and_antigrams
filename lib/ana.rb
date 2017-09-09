c41ee005fd01a7909c6bf72667ca778ab466e971
Riki Montgomery <mostriki820@gmail.com>
2017-09-09 11:08:17 -0700

    refactor order of operations

--- lib/ana.rb
+++ lib/ana.rb
@@ -8,27 +8,36 @@ class Anagrams
 
 # checks to see if it is a word.
   def ana (input, comparison)
+
+# splits into an array of lowercase characters.
+    input_arr = input.downcase.gsub(/[^a-z0-9]/i, '').split(//)
+    comparison_arr = comparison.downcase.gsub(/[^a-z0-9]/i, '').split(//)
+
+# counts the vowels in a string
     input_count = input.count('aeiouy')
     comparison_count = comparison.count('aeiouy')
+
     if input_count == 0 || comparison_count == 0
       return "You need to input actual words!"
     end
-# splits into an array of lowercase characters.
-    input = input.downcase.gsub(/[^a-z0-9]/i, '').split(//)
-    comparison = comparison.downcase.gsub(/[^a-z0-9]/i, '').split(//)
+
 # checks to see if it is a palindrome.
-    if input.reverse == comparison
+    if input_arr.reverse == comparison_arr
       return "Congratulations. Your anagrams make a palindrome."
     end
+
 # sorts into alphabetical array of characters.
-    input_arr = input.sort
-    comparison_arr = comparison.sort
+    input_arr = input_arr.sort
+    comparison_arr = comparison_arr.sort
+
 # compares arrays for matching characters
     antigram_check = input_arr & comparison_arr
+
 # checks to see if word is an antigram instead.
     if antigram_check.empty? == true
       return "These words have no letter matches and are antigrams."
     end
+
 # checks to see if it is an anagram.
     if input_arr == comparison_arr
       true
