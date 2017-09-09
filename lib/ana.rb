ba0f6dd8d439aa2fb726745980ad18a24b57e8e2
Riki Montgomery <mostriki820@gmail.com>
2017-09-09 16:38:41 -0700

    Create a method to check for palindromes

--- lib/ana.rb
+++ lib/ana.rb
@@ -1,19 +1,25 @@
 #!/usr/bin/env ruby
 
 class Anagrams
+# initilization method for inputs
   def initialize(input, comparison)
     @input = input
     @comparison = comparison
   end
 
+# method (check words for vowels)
   def word_check (word)
     word.count('aeiouy') > 0
   end
 
-# checks to see if it is a word.
-  def ana
+# method (check if words make a palindrome)
+  def palindrom_check (word1, word2)
+    word1.reverse == word2
+  end
 
-# splits into an array of lowercase characters.
+# method (check if words are anagrams)
+  def ana
+# splits into an array of lowercase letters.
     input_arr = @input.downcase.gsub(/[^a-z0-9]/i, '').split(//)
     comparison_arr = @comparison.downcase.gsub(/[^a-z0-9]/i, '').split(//)
 
@@ -23,7 +29,7 @@ class Anagrams
     end
 
 # checks to see if it is a palindrome.
-    if input_arr.reverse == comparison_arr
+    if palindrom_check(input_arr, comparison_arr) == true
       return "Congratulations. Your anagrams make a palindrome."
     end
 
