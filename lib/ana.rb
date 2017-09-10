b310eb9e342f6643eaf864eed73d11e2e4e97601
Riki Montgomery <mostriki820@gmail.com>
2017-09-09 18:21:21 -0700

    refactor code to make more methods, change order of operations, and update descriptions

--- lib/ana.rb
+++ lib/ana.rb
@@ -5,6 +5,10 @@ class Anagrams
   def initialize(input, comparison)
     @input = input
     @comparison = comparison
+    @input_arr = @input.downcase.gsub(/[^a-z0-9]/i, '').split(//)
+    @comparison_arr = @comparison.downcase.gsub(/[^a-z0-9]/i, '').split(//)
+    @input_sort = @input_arr.sort
+    @comparison_sort = @comparison_arr.sort
   end
 
 # method (check words for vowels)
@@ -13,40 +17,31 @@ class Anagrams
   end
 
 # method (check if words make a palindrome)
-  def palindrom_check (word1, word2)
-    word1.reverse == word2
+  def palindrom_check (input, comparison)
+    input.reverse == comparison
+  end
+
+# method (check if words are antigrams)
+  def antigram_check(input, comparison)
+    (@input_sort & @comparison_sort).empty?
   end
 
 # method (check if words are anagrams)
   def ana
-# splits into an array of lowercase letters.
-    input_arr = @input.downcase.gsub(/[^a-z0-9]/i, '').split(//)
-    comparison_arr = @comparison.downcase.gsub(/[^a-z0-9]/i, '').split(//)
-
-# counts the vowels in a string
+# checks for words
     if word_check(@input) == false || word_check(@comparison) == false
       return "You need to input actual words!"
     end
 
 # checks to see if it is a palindrome.
-    if palindrom_check(input_arr, comparison_arr) == true
+    if palindrom_check(@input_arr, @comparison_arr) == true
       return "Congratulations. Your anagrams make a palindrome."
     end
 
-# sorts into alphabetical array of characters.
-    input_arr = input_arr.sort
-    comparison_arr = comparison_arr.sort
-
-# compares arrays for matching characters
-    antigram_check = input_arr & comparison_arr
-
-# checks to see if word is an antigram instead.
-    if antigram_check.empty? == true
+# checks to see if words make an anagram or an antigram.
+    if antigram_check(@input, @comparison) == true
       return "These words have no letter matches and are antigrams."
-    end
-
-# checks to see if it is an anagram.
-    if input_arr == comparison_arr
+    elsif @input_sort == @comparison_sort
       true
     else
       false
